5940cd2ce928dde716a1ef78da999359d58b2f34
Aj-gupta <gajay3954@gmail.com>
2020-10-31 10:46:28 +0530

    mongoose part-2

---
+++ index.js
@@ -0,0 +1,32 @@
+// mongod --dbpath=data --bind_ip 127.0.0.1
+
+
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db)=>{
+    console.log('Connected correctly to a server');
+
+    Dishes.create({
+        name:'Uthappizza',
+        description:'test'
+    })
+        .then((dish)=> {
+            console.log(dish);
+           return Dishes.find({}).exec();
+        })
+        .then((dishes)=>{
+            console.log(dishes);
+            return Dishes.remove({});
+        })
+        .then(()=>{
+            return mongoose.connection.close();
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
+});
